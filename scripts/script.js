a113043048ec2184487d16fc4877ddf9b82ea33c
lenizza1331 <leniyuva@gmail.com>
2022-01-13 18:25:52 +0300

    added sticky top on scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//animation for my photo
 let myPhoto = document.querySelector("#myPhoto");
 let myPhotoArray = ['https://cdn.glitch.me/2a3696f3-76a8-4bc5-8e54-9b355f7d5d50/lz%20(25).png?v=1639966299494',
 'https://cdn.glitch.me/2a3696f3-76a8-4bc5-8e54-9b355f7d5d50/lz%20(22).png?v=1639966299592',
@@ -23,6 +24,7 @@ myPhoto.addEventListener("click", ()=> {
 });
 
 
+//animation for my scroll arrow
 const scrollTop = document.querySelector('#scrollTop');
 window.onscroll = () => {
     if (window.scrollY > 600){
@@ -35,5 +37,28 @@ scrollTop.onclick = ()=>{
     window.scrollTo(0,0);
 }
 
+
+//animation for navbar on scroll
+
+let lastScroll = 0;
+const defaultOffset = 150;
+const header = document.querySelector('.navbar');
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('sticky-top');
+
+window.addEventListener('scroll', () => {
+    if(scrollPosition() > lastScroll && containHide() ) {
+        //scroll down
+        header.classList.remove('sticky-top');
+        
+    }
+    else if(scrollPosition() < lastScroll && !containHide()  && scrollPosition() > defaultOffset){
+        //scroll up
+        header.classList.add('sticky-top');
+        
+    }
+    lastScroll = scrollPosition();
+})
 
 
